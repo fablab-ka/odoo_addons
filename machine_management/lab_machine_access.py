f1f7d834d9adf33f4de7e8e6e8f5d80c53ed9e87
Philip Caroli <philip@caroli.de>
2017-07-30 16:50:56 +0200

    fixed type (date->data)

--- machine_management/lab_machine_access.py
+++ machine_management/lab_machine_access.py
@@ -12,16 +12,6 @@ class machine_job(models.Model):
     start_time = fields.Datetime(string="Start time", required=True)
     end_time = fields.Datetime(string="End time", required=True)
     sale_order_id = fields.Many2one(string="Sale Order", comodel_name="sale.order")
-    duration = fields.Float(string="Duration (min.)", compute='compute_duration', store=True, default=0)
+    duration = fields.Float(string="Duration (min.)", store=True, default=0)
     info = fields.Text(string="Info")
-
-    @api.depends('start_time', 'end_time')
-    def compute_duration(self):
-        try:
-            start = fields.Datetime.from_string(self.start_time)
-            end = fields.Datetime.from_string(self.end_time)
-            seconds = (end  - start).total_seconds()
-            self.duration = seconds / 60
-        except:
-            self.duration = 0.0
 
