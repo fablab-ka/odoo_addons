312ffebaf4f72e422a5e2e5b6835fe4eb1455b54
Philip Caroli <philip@caroli.de>
2017-08-30 11:14:25 +0200

    Fixed typo: res.user -> res.users

--- machine_management/lab_machine.py
+++ machine_management/lab_machine.py
@@ -9,7 +9,7 @@ class lab_machine(models.Model):
     name = fields.Char(string="Name", required=True)
     status = fields.Selection([('r', 'running'), ('o', 'out of order'), ('n', 'not ready'), ('t', 'test phase')], string="Machine Status", required=True, default='n')
     rules = fields.Selection([('f', 'free for all'), ('r', 'restricted'), ('n', 'no access')], string="Access Rules", required=True, default='n')
-    machine_user = fields.Many2one(string="Machine User", comodel_name="res.user", help="assigned machine user")
+    machine_user = fields.Many2one(string="Machine User", comodel_name="res.users", help="assigned machine user")
     user_ids = fields.Many2many(string="Users", comodel_name="res.partner", relation="machine_user_relation", column1="machine_id", column2="user_id")
     owner_ids = fields.Many2many(string="Owners", comodel_name="res.partner", relation="machine_owner_relation", column1="machine_id", column2="owner_id")
     machine_tag_1 = fields.Many2one(comodel_name='product.tag', string="Product Tag 1",
