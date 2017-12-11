02e3ceda01cf5192d8fcddbfeed605ba0de6cd03
Philip Caroli <philip@caroli.de>
2017-12-11 19:08:48 +0100

    Added webcontroller file to git

---
+++ more_views/main.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+
+import babel.dates
+import re
+import werkzeug
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+
+from odoo import fields, http, _
+from odoo.addons.website.models.website import slug
+from odoo.http import request
+
+
+class WebsiteEventController(http.Controller):
+
+    @http.route(['/event_list'], type='http', auth="public", website=True)
+    def events(self, **searches):
+        Event = request.env['event.event']
+        EventType = request.env['event.type']
+
+        searches.setdefault('date', 'all')
+        searches.setdefault('type', 'all')
+        searches.setdefault('country', 'all')
+
+        domain_search = {}
+
+        def sdn(date):
+            return fields.Datetime.to_string(date.replace(hour=23, minute=59, second=59))
+
+        def sd(date):
+            return fields.Datetime.to_string(date)
+        today = datetime.today()
+        dates = [
+            ['all', _('Next Events'), [("date_end", ">", sd(today))], 0],
+            ['today', _('Today'), [
+                ("date_end", ">", sd(today)),
+                ("date_begin", "<", sdn(today))],
+                0],
+            ['week', _('This Week'), [
+                ("date_end", ">=", sd(today + relativedelta(days=-today.weekday()))),
+                ("date_begin", "<", sdn(today + relativedelta(days=6-today.weekday())))],
+                0],
+            ['nextweek', _('Next Week'), [
+                ("date_end", ">=", sd(today + relativedelta(days=7-today.weekday()))),
+                ("date_begin", "<", sdn(today + relativedelta(days=13-today.weekday())))],
+                0],
+            ['month', _('This month'), [
+                ("date_end", ">=", sd(today.replace(day=1))),
+                ("date_begin", "<", (today.replace(day=1) + relativedelta(months=1)).strftime('%Y-%m-%d 00:00:00'))],
+                0],
+            ['nextmonth', _('Next month'), [
+                ("date_end", ">=", sd(today.replace(day=1) + relativedelta(months=1))),
+                ("date_begin", "<", (today.replace(day=1) + relativedelta(months=2)).strftime('%Y-%m-%d 00:00:00'))],
+                0],
+            ['old', _('Old Events'), [
+                ("date_end", "<", today.strftime('%Y-%m-%d 00:00:00'))],
+                0],
+        ]
+
+        # search domains
+        # TDE note: WTF ???
+        current_date = None
+        current_type = None
+        current_country = None
+        for date in dates:
+            if searches["date"] == date[0]:
+                domain_search["date"] = date[2]
+                if date[0] != 'all':
+                    current_date = date[1]
+
+        if searches["type"] != 'all':
+            current_type = EventType.browse(int(searches['type']))
+            domain_search["type"] = [("event_type_id", "=", int(searches["type"]))]
+
+        if searches["country"] != 'all' and searches["country"] != 'online':
+            current_country = request.env['res.country'].browse(int(searches['country']))
+            domain_search["country"] = ['|', ("country_id", "=", int(searches["country"])), ("country_id", "=", False)]
+        elif searches["country"] == 'online':
+            domain_search["country"] = [("country_id", "=", False)]
+
+        def dom_without(without):
+            domain = [('state', "in", ['draft', 'confirm', 'done'])]
+            for key, search in domain_search.items():
+                if key != without:
+                    domain += search
+            return domain
+
+        # count by domains without self search
+        for date in dates:
+            if date[0] != 'old':
+                date[3] = Event.search_count(dom_without('date') + date[2])
+
+        domain = dom_without('type')
+        types = Event.read_group(domain, ["id", "event_type_id"], groupby=["event_type_id"], orderby="event_type_id")
+        types.insert(0, {
+            'event_type_id_count': sum([int(type['event_type_id_count']) for type in types]),
+            'event_type_id': ("all", _("All Categories"))
+        })
+
+        domain = dom_without('country')
+        countries = Event.read_group(domain, ["id", "country_id"], groupby="country_id", orderby="country_id")
+        countries.insert(0, {
+            'country_id_count': sum([int(country['country_id_count']) for country in countries]),
+            'country_id': ("all", _("All Countries"))
+        })
+
+        step = 10  # Number of events per page
+        event_count = Event.search_count(dom_without("none"))
+        # pager = request.website.pager(
+        #     url="/event_list",
+        #     url_args={'date': searches.get('date'), 'type': searches.get('type'), 'country': searches.get('country')},
+        #     total=event_count,
+        #     page=page,
+        #     step=step,
+        #     scope=5)
+
+        order = 'website_published desc, date_begin'
+        if searches.get('date', 'all') == 'old':
+            order = 'website_published desc, date_begin desc'
+        events = Event.search(dom_without("none"), limit=step, order=order)
+
+        values = {
+            'current_date': current_date,
+            'current_country': current_country,
+            'current_type': current_type,
+            'event_ids': events,  # event_ids used in website_event_track so we keep name as it is
+            'dates': dates,
+            'types': types,
+            'countries': countries,
+            # 'pager': pager,
+            'searches': searches,
+            'search_path': "?%s" % werkzeug.url_encode(searches),
+        }
+
+        return request.render("more_views.event_index", values)
